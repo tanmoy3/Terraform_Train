4a3be2ccf3f897a6d25c4233b02c9e92b8db4cdf
Tanmoy Mandal <90269115+tanmoy3@users.noreply.github.com>
2025-11-20 01:57:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
 provider "aws" {
-    region = "us-east-1"
-    
+    region = "us-east-1"    
 }
 
-<<<<<<< HEAD
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-test-bucket-191120251024"
   tags = {
       Name  = "my_bucket"
-=======
-resource "aws_s3_bucket" "My_bucket1" {
-  bucket = "my-test-bucket-191120251024"
-  tags = {
-      Name  = "My_bucket1"
->>>>>>> 5bf613d (check rebase, update main.tf)
   }
 } 
