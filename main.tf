d2ce74d2dccd39095d9b251e3200f7e9a87ed674
Tanmoy Mandal <mandaltanmoy264@gamil.com>
2025-11-19 23:44:17 +0530

    create s3 bucket on AWS

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+    
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-test-bucket-191120251024"
+
+  tags = {
+    Name        = "My bucket"
+  }
+} 
