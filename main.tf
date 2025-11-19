b4b06fc257b001c42a2b1c013fde496e90c03fe8
Tanmoy Mandal <90269115+tanmoy3@users.noreply.github.com>
2025-11-20 01:58:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
     region = "us-east-1"    
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "my_bucket1" {
   bucket = "my-test-bucket-191120251024"
   tags = {
-      Name  = "my_bucket"
+      Name  = "my_bucket1"
   }
 } 
