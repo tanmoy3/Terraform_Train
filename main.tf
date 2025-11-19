fcf4ee0910950614d7468b836567456a99b91318
Tanmoy Mandal <mandaltanmoy264@gamil.com>
2025-11-20 01:47:40 +0530

    check rebase, update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,16 @@ provider "aws" {
     
 }
 
+<<<<<<< HEAD
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-test-bucket-191120251024"
   tags = {
       Name  = "my_bucket"
+=======
+resource "aws_s3_bucket" "My_bucket1" {
+  bucket = "my-test-bucket-191120251024"
+  tags = {
+      Name  = "My_bucket1"
+>>>>>>> 5bf613d (check rebase, update main.tf)
   }
 } 
